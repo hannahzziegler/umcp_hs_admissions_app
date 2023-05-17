4773f41c8c3f2b43ac48e7559606ec2041dff83e
Hannah Ziegler <95310885+hannahzziegler@users.noreply.github.com>
2023-05-17 03:44:47 +0000

    search, database, app template

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+from peewee import *
 app = Flask(__name__)
 
-@app.route("/")
+db = SqliteDatabase('high_school_admissions.db')
+
+class Admissions(Model):
+    id = IntegerField(unique=True)
+    school_id = IntegerField()
+    school = CharField()
+    year = CharField()
+    applied = CharField()
+    admitted = CharField()
+    acceptance_rate = CharField()
+    lat = IntegerField()
+    lon = IntegerField()
+    city = CharField()
+    county = CharField()
+    state = CharField()
+    zip = CharField()
+    classification = CharField()
+
+    class Meta:
+        table_name = "admissions"
+        database = db
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    template = 'index.html'
-    return render_template(template)
+    if request.method == 'POST':
+        search_term = request.form['search_term']
+        query = (
+            Admissions
+            .select()
+            .where(fn.lower(Admissions.school).contains(search_term.lower()))
+            .order_by(Admissions.year)
+        )
+        results = list(query)
+    else:
+        results = None
+        search_term = None
+        # Query database for data based on search term
+
+    return render_template('index.html', search_term=search_term, results=results)
+    
 
 if __name__ == '__main__':
     # Fire up the Flask test server
