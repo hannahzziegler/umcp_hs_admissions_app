d85e757e5b9f0582686b0652b853d26298b8d3cf
Hannah Ziegler <95310885+hannahzziegler@users.noreply.github.com>
2023-05-17 04:58:25 +0000

    yayay working search!

--- app.py
+++ app.py
@@ -26,21 +26,68 @@ class Admissions(Model):
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    counties = [
+        "Allegany County",
+        "Anne Arundel County",
+        "Baltimore County",
+        "Calvert County",
+        "Caroline County",
+        "Carroll County",
+        "Cecil County",
+        "Charles County",
+        "Dorchester County",
+        "Frederick County",
+        "Garrett County",
+        "Harford County",
+        "Howard County",
+        "Kent County",
+        "Montgomery County",
+        "Prince George's County",
+        "Queen Anne's County",
+        "St. Mary's County",
+        "Somerset County",
+        "Talbot County",
+        "Washington County",
+        "Wicomico County",
+        "Worcester County"
+    ]
+
     if request.method == 'POST':
         search_term = request.form['search_term']
+        county_filter = request.form['county']
+
         query = (
             Admissions
             .select()
-            .where(fn.lower(Admissions.school).contains(search_term.lower()))
-            .order_by(Admissions.year)
+            .where(
+                (fn.lower(Admissions.school).contains(search_term.lower())) &
+                (Admissions.county.contains(county_filter))
+            )
+            .order_by(Admissions.county, Admissions.school, Admissions.year)
         )
-        results = list(query)
+
+        results = []
+        current_school = None
+        current_school_results = []
+
+        for result in query:
+            if result.school != current_school:
+                if current_school:
+                    results.append((current_school, current_school_results))
+                current_school = result.school
+                current_school_results = []
+            current_school_results.append(result)
+
+        if current_school:
+            results.append((current_school, current_school_results))
+
     else:
         results = None
         search_term = None
+        county_filter = None
         # Query database for data based on search term
 
-    return render_template('index.html', search_term=search_term, results=results)
+    return render_template('index.html', search_term=search_term, results=results, county_filter=county_filter, counties=counties)
     
 
 if __name__ == '__main__':
